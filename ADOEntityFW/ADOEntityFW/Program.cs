08b164c125ab1a57ed8bbcb3eeee90a2ca4e902d
Mukul Joshi <mmjoshi79@gmail.com>
2014-04-26 22:38:18 +0530

    ADO.NET Entity Framework Example

---
+++ ADOEntityFW/ADOEntityFW/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADOEntityFW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myCycleZoneEntities en = new myCycleZoneEntities();
+            var email = from cs in en.Users where cs.userName == "mmjoshi79@gmail.com" 
+                           select cs;
+            User mathCourse = email.FirstOrDefault<User>();
+            IList<User> courseList = email.ToList<User>();
+
+            string courseName = mathCourse.FirstName.ToString();
+
+            Console.WriteLine("Email ID: " + courseName);
+        }
+    }
+}
